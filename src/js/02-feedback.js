f3815b026d30069fa667bf22ce976febaa2ec355
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-07-03 11:58:33 +0300

    Updated files

---
+++ src/js/02-feedback.js
@@ -0,0 +1,19 @@
+import '../css/common.css';
+import '../css/feedback-form.css';
+
+const refs = {
+    form: document.querySelector('js-feedback-form'),
+    textarea: document.querySelector('js-feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+function onFormSubmit(evt) {}
+
+function onTextareaInput(evt) {
+    const value = evt.currentTarget.value;
+    console.log(value);
+}
+
+function populateMessageOutput() {}
